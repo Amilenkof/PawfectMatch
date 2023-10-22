002f8ef10d2a5be485f22884586f5b8959a215d5
Amilenkof <f.o.g@list.ru>
2023-10-22 16:05:21 +0400

    -написал тесты для MessageSupplier

---
+++ src/test/java/pro/sky/telegrambot/serviceTest/keyboards/KeyBoardServiceTest.java
@@ -0,0 +1,12 @@
+package pro.sky.telegrambot.serviceTest.keyboards;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pro.sky.telegrambot.service.ShelterService;
+import pro.sky.telegrambot.service.VolunteerService;
+
+@ExtendWith(MockitoExtension.class)
+public class KeyBoardServiceTest {
+
+}
